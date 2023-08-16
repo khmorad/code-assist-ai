af753ed1e9cf5664927dc671dd582e2a626a984f
woddyowl <107232768+woddyowl@users.noreply.github.com>
2023-08-16 12:29:49 -0700

    fixed some code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  // State to hold the contents of the uploaded file
+  const [fileContent, setFileContent] = useState('');
+
+  // Function to handle file change event
+  const handleFileChange = (event) => {
+    // Get the selected file from the input
+    const file = event.target.files[0];
+
+    // Check if a file was selected
+    if (file) {
+      // Create a FileReader instance
+      const reader = new FileReader();
+
+      // Set up the onload event handler
+      reader.onload = (e) => {
+        // Update the fileContent state with the read contents
+        setFileContent(e.target.result);
+      };
+
+      // Read the contents of the file as text
+      reader.readAsText(file);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>File Reader App</h1>
+      
+      {/* Input element for selecting a text file */}
+      <input type="file" accept=".js, .py, .css" onChange={handleFileChange} />
+      
+      <div>
+        <h2>File Content:</h2>
+        {/* Display the contents of the selected file */}
+        <pre>{fileContent}</pre>
+      </div>
     </div>
   );
 }
